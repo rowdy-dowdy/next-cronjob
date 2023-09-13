a86ed71c03ef671903d08f6ce12ebf856c9f6cfa
Viet Hung <viet.hung.2898@gmail.com>
2023-09-13 21:55:33 +0700

    add

--- components/web/layouts/WebRootLayout.tsx
+++ components/web/layouts/WebRootLayout.tsx
@@ -55,15 +55,35 @@ const WebRootLayout = ({ children }: {
 
         <div 
           ref={screenEl} 
-          className={`${styles.screen} text-gray-200 flex flex-col`}
+          className={`${styles.screen} text-gray-200 flex flex-col space-y-6`}
         >
           <div className="flex-none flex flex-col items-center justify-center space-y-2">
             <Image src="/images/logo.svg" alt="logo" width={200} height={200} className="h-28 w-auto" />
             <div className="text uppercase font-semibold">Tam sao thất bản</div>
           </div>
-          <div className="flex-grow min-h-0"></div>
-          <div className="flex-none flex items-center justify-center text-gray-300">
-            <Image src="/images/logo.svg" alt="logo" width={200} height={200} className="h-28 w-auto" />
+
+          <div className="flex-grow min-h-0 flex space-x-6">
+            <div className="w-3/5 rounded-lg border-[3px] border-[--border] p-4"></div>
+            <div className="w-2/5 rounded-lg border-[3px] border-[--border] p-4 text-center">
+              <div className="text text-xl font-semibold text-teal-300 uppercase">Cách chơi</div>
+              <style jsx>{`
+                .loader {
+                  
+                }
+              `}</style>
+              <div className="icon-svg w-7 h-7">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" className="w-full h-full">
+                  <circle cx="100" cy="100" r="90" stroke="currentColor" strokeWidth="20" fill="none" stroke-linecap="round">
+                    <animate attributeName="stroke-dasharray" from="0 565.48" to="565.48 0" dur="2s" />
+                  </circle>
+                </svg>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="flex-none flex items-center justify-between text-white/50 font-semibold">
+            <Image src="/logo.png" alt="logo" width={200} height={200} className="h-10 w-auto" />
             <span>© 2023 <a href="https://github.com/rowdy-dowdy" target="_blank" className="hover:text-blue-600">Việt Hùng Ít</a>. All Rights Reserved.</span>
           </div>
         </div>
