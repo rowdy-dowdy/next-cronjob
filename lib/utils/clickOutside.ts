e3da714b851091d3f63bdde7890dd48d9ea91b05
Viet Hung <viet.hung.2898@gmail.com>
2023-09-13 17:41:20 +0700

    add

--- lib/utils/clickOutside.ts
+++ lib/utils/clickOutside.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 
 const useClickOutside = (ref: any, cb: () => void) => {
   useEffect(() => {
@@ -19,4 +19,29 @@ const useClickOutside = (ref: any, cb: () => void) => {
   }, [ref]);
 }
 
-export {useClickOutside}
+function useResizeObserver<T extends HTMLElement>(
+  callback: (target: T, entry: ResizeObserverEntry) => void
+) {
+  const ref = useRef<T>(null)
+
+  useLayoutEffect(() => {
+    const element = ref?.current
+
+    if (!element) {
+      return
+    }
+
+    const observer = new ResizeObserver((entries) => {
+      callback(element, entries[0])
+    })
+
+    observer.observe(element)
+    return () => {
+      observer.disconnect()
+    }
+  }, [callback, ref])
+
+  return ref
+}
+
+export { useClickOutside, useResizeObserver }
